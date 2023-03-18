b2e451b4e64ecc082d7e7b52ffd1cfac9a48ad75
Marcin Szymura <marcin.szymura@gmail.com>
2023-03-18 23:59:59 +0100

    Day 10 part 1

---
+++ day10-1.groovy
@@ -0,0 +1,177 @@
+def testInput = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+def isRelevant(cycleNumber) {
+    (cycleNumber - 20) % 40 == 0 && cycleNumber <= 220
+}
+
+def sumOfSignalStrengths(List<String> lines) {
+    def totalStrength = 0
+    def x = 1
+    def cycleNumber = 1
+    lines.each {
+        if(isRelevant(cycleNumber)) {
+            totalStrength += x*cycleNumber
+        }
+        if(it == 'noop') {
+            cycleNumber++
+        }
+        else {
+            def amount = (it - 'addx ') as int
+            if(isRelevant(cycleNumber+1)) {
+                totalStrength += x*(cycleNumber+1)
+            }
+            x += amount
+            cycleNumber += 2
+        }   
+    }
+    totalStrength
+}
+
+assert 13140 == sumOfSignalStrengths(testInput.split('\n') as List)
+println sumOfSignalStrengths(new File('input/day10.txt').readLines())
